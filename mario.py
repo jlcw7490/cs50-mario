a9d945e4c6cecba825f31626f676b14e7e188ed4
Justin Liu <66164478+jlcw7490@users.noreply.github.com>
2020-11-13 15:51:58 +0800

    Create mario.py

---
+++ mario.py
@@ -0,0 +1,8 @@
+echo "# cs50-mario.py" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jlcw7490/cs50-mario.py.git
+git push -u origin main
+                
